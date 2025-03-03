7e21bfab9e7bd8fd91afc2f54c429a061bbc8279
can gündoğdu <gundogdu.can@hotmail.com>
2025-03-03 23:55:10 +0300

    03.03.2025

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,30 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz.");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında kısmını boş geçemezsiniz.");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Ünvan kısmını boş geçemezsiniz.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("En az 2 karekter giriniz!");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("En az 2 karekter giriniz!");
+            RuleFor(x => x.WriterTitle).MinimumLength(2).WithMessage("En az 2 karekter giriniz!");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("En fazla 50 karekter giriniz!");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("En fazla 50 karekter giriniz!");
+            RuleFor(x => x.WriterTitle).MaximumLength(50).WithMessage("En fazla 50 karekter giriniz!");
+
+        }
+        
+    }
+}
--- Entity/Concrete/Writer.cs
+++ Entity/Concrete/Writer.cs
@@ -11,6 +11,8 @@ namespace Entity.Concrete
     {
         [Key]
         public int WriterId { get; set; }
+        [StringLength(50)]
+        public string WriterTitle { get; set; }
 
         [StringLength(50)]
         public string WriterName { get; set; }
@@ -21,10 +23,13 @@ namespace Entity.Concrete
         [StringLength(100)]
         public string WriterImage { get; set; }
 
-        [StringLength(50)]
+        [StringLength(100)]
+        public string WriterAbout { get; set; }
+
+        [StringLength(200)]
         public string WriterEmail { get; set; }
 
-        [StringLength(20)]
+        [StringLength(200)]
         public string WriterPassword { get; set; }
 
         public ICollection<Heading> Headings { get; set; }
