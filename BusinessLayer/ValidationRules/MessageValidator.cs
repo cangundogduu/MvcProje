dc2b12e66a6ca6028477ca8d9ee475892aa7a444
can gündoğdu <gundogdu.can@hotmail.com>
2025-03-15 20:51:49 +0300

    15.03.2025

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,25 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator: AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı Boş Geçilemez.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu Boş Geçilemez.");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesaj İçeriği Boş Geçilemez.");
+            RuleFor(x => x.ReceiverMail).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi yazınız.");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz.");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("En fazla 100 karakter girebilirsiniz.");
+
+        }
+    }
+}
---
+++ MvcProje/Controllers/MessageController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messageList = messageManager.GetListInbox();
+            return View(messageList);
+        }
+
+        public ActionResult Sendbox() 
+        {
+            var messageList= messageManager.GetListSendbox();
+            return View(messageList);
+        }
+
+
+
+        public ActionResult NewMessage() 
+        {
+        
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message message) 
+        {
+            return RedirectToAction(nameof(Inbox));
+        }
+    }
+}
